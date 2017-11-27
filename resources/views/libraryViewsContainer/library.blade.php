93bf39e4de0b57605757e6a8f4cd4217eceee039
Mohammad Salah El-Deen <M.Salah@SEHA.ORG>
2017-11-27 13:47:29 +0200

    first project

---
+++ resources/views/libraryViewsContainer/library.blade.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Library</title>
+  <meta charset="utf-8">
+<link rel="stylesheet"  type="text/css" href="{{asset('css/bootstrap.min.css')}}">
+<style type="text/css">
+body {
+	background-image: url('images/library.jpg');
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-position: center;
+	background-size:100% auto ;
+
+	
+}
+header {opacity:0.7 ;}
+footer {background-color:#fff ;opacity:0.9 ;text-align:center;}
+	
+
+</style>
+</head>
+<body>
+<header class="jumbotron">
+  <div class="container">
+     <h1>The Book store</h1>
+	 <p> reading a good book is taking a journy</p>
+  </div>
+</header>
+<div class="container" style="opacity:09"> 
+    <div class="row">
+	    <div class="col-md-3"> 
+		    <div class="thumbnail" > 
+			<img src="{{asset('images/history.jpg')}}">
+			<h1><a class="btn btn-primary">History</a></h1>
+			</div>
+        </div>	
+		<div class="col-md-3"> 
+		    <div class="thumbnail"> 
+			<img src="{{asset('images/science.jpg')}}">
+			<h1><a class="btn btn-primary">science</a></h1>
+			</div>
+        </div>
+		<div class="col-md-3"> 
+		    <div class="thumbnail"> 
+			<img src="{{asset('images/electrical.jpg')}}">
+			<h1><a class="btn btn-primary">electrical</a></h1>
+			</div>
+        </div>
+		<div class="col-md-3"> 
+		    <div class="thumbnail"> 
+			<img src="{{asset('images/electronics.jpg')}}">
+			<h1><a class="btn btn-primary">electronics</a></h1>
+			</div>
+        </div>
+		<div class="col-md-3"> 
+		    <div class="thumbnail"> 
+			<img src="{{asset('images/electronics.jpg')}}">
+			<h1><a class="btn btn-primary">electronics</a></h1>
+			</div>
+        </div>
+		<div class="col-md-3"> 
+		    <div class="thumbnail"> 
+			<img src="{{asset('images/electronics.jpg')}}">
+			<h1><a class="btn btn-primary">electronics</a></h1>
+			</div>
+        </div>
+		<div class="col-md-3"> 
+		    <div class="thumbnail"> 
+			<img src="{{asset('images/electronics.jpg')}}">
+			<h1><a class="btn btn-primary">electronics</a></h1>
+			</div>
+        </div>
+		<div class="col-md-3"> 
+		    <div class="thumbnail"> 
+			<img src="{{asset('images/electronics.jpg')}}">
+			<h1><a class="btn btn-primary">electronics</a></h1>
+			</div>
+        </div>
+		<div class="col-md-3"> 
+		    <div class="thumbnail"> 
+			<img src="{{asset('images/electrical.jpg')}}">
+			<h1><a class="btn btn-primary">electrical</a></h1>
+			</div>
+        </div>
+		<div class="col-md-3"> 
+		    <div class="thumbnail"> 
+			<img src="{{asset('images/electrical.jpg')}}">
+			<h1><a class="btn btn-primary">electrical</a></h1>
+			</div>
+        </div>
+		<div class="col-md-3"> 
+		    <div class="thumbnail"> 
+			<img src="{{asset('images/electrical.jpg')}}">
+			<h1><a class="btn btn-primary">electrical</a></h1>
+			</div>
+        </div>
+		<div class="col-md-3"> 
+		    <div class="thumbnail"> 
+			<img src="{{asset('images/electrical.jpg')}}">
+			<h1><a class="btn btn-primary">electrical</a></h1>
+			</div>
+        </div>
+	 </div>
+</div>
+<footer class ="container">
+&copy; All Right Reserved for safa
+</footer>
+
+
+
+</body>
+</html>
